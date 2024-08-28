64c2301038b3ecf4b5c519b88c9718b0169b1564
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-08-28 11:12:46 -0300

    install hotreload

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+require('dotenv').config();
+
 
 const app = express();
 const port = 3000;
@@ -6,9 +8,12 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send({
+    "api-key":process.env.GEMINI_API_KEY
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
